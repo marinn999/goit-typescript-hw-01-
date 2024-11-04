579273b60dd24b03db0f8a8fc87fd9093fd344af
marinn999 <marinacompel@icloud.com>
2024-11-04 09:22:41 +0100

    fixed

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,3 +1,5 @@
-function merge<T, U>(objA: T, objB: U): T & U {
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
   return Object.assign({}, objA, objB) as T & U;
 }
+//`extends object` варто зробити для того, щоб обмежети дженерік тип можливістю передачі даних об'єкного типу.
+//В функції використовується метод конкатинації двох об'єктів.
